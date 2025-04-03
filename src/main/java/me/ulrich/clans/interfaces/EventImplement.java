5010f9f1415e1fa33f945d6924122c44b6effbd3
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2025-04-03 20:33:11 -0300

    Update EventImplement.java

--- src/main/java/me/ulrich/clans/interfaces/EventImplement.java
+++ src/main/java/me/ulrich/clans/interfaces/EventImplement.java
@@ -1,6 +1,7 @@
 package me.ulrich.clans.interfaces;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -9,11 +10,15 @@ public interface EventImplement {
 	
 	boolean playerInEvent(Player player);
 	
+	boolean playerInEvent(UUID playerUUID);
+	
 	boolean locationIsEvent(Location location);
 	
 	Optional<String> getLocationEventName(Location location);
 	
 	Optional<String> getPlayerEventName(Player player);
+	
+	Optional<String> getPlayerEventName(UUID playerUUID);
 
 	Optional<String> getPluginVersion();
 	
