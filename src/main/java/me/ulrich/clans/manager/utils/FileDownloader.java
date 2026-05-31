d2d46cc9d24172bbb8371391cf29209a7234b25b
UlrichBR <unixmc@hotmail.com>
2026-05-31 13:11:03 -0300

    [+]Changed location of FileDownloader

---
+++ src/main/java/me/ulrich/clans/manager/utils/FileDownloader.java
@@ -0,0 +1,23 @@
+package me.ulrich.clans.manager.utils;
+
+
+import me.ulrich.clans.Clans;
+
+public class FileDownloader {
+
+    public FileDownloader onSuccess(Runnable onSuccess) {
+		return null;
+    }
+
+    public FileDownloader onFailure(Runnable onFailure) {
+		return null;
+    }
+
+    public void downloadUsingLocale(final Clans clan, final String urlStr, final String file) {
+
+    }
+
+    public void successDownload() {}
+    public void failedDownload() {}
+
+}
---
+++ src/main/java/me/ulrich/clans/manager/utils/Files.java
@@ -0,0 +1,40 @@
+package me.ulrich.clans.manager.utils;
+
+
+import java.io.File;
+import java.io.InputStream;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+
+public class Files {
+
+	
+	public Files(String path, String name, String resource, String extension) {
+	
+	}
+	
+	public FileConfiguration get() {
+		return null;
+	}
+	
+	public File getFile() {
+		return null;
+	}
+	
+	public void save() {
+
+	}
+	
+	public void reload() {
+
+	}
+	
+	public static void copy(InputStream is, File file) {
+
+	}
+	
+	public Files getFileManager() {
+		return this;
+	}
+}
