121552dc42ab33e3a8dc751fc5c841494fe5cf7a
UlrichBR <unixmc@hotmail.com>
2026-08-12 12:46:21 -0300

    [+] Final versioning updated.

--- src/main/java/me/ulrich/clans/manager/utils/VersionUtil.java
+++ src/main/java/me/ulrich/clans/manager/utils/VersionUtil.java
@@ -2,59 +2,72 @@ package me.ulrich.clans.manager.utils;
 
 import org.jetbrains.annotations.NotNull;
 
+import me.ulrich.clans.manager.utils.VersionUtil.VersionEnum;
+
 public class VersionUtil {
 
-    public static String VERSION;
+	public static String VERSION;
 
-    public static String CLEAN_VERSION;
-    
-    public static Integer BUILD_VERSION;
+	public static String CLEAN_VERSION;
 
-    static {
+	public static Integer BUILD_VERSION;
 
-    }
-    
-    
-    public static boolean is(@NotNull VersionEnum ve) {
-    	return false;
-    }
-    
+	static {
 
-    public static boolean isCompatible(@NotNull VersionEnum ve){
-    	return false;
-    }
+	}
 
-    public static boolean isAbove(@NotNull VersionEnum ve) {
-    	return false;
-    }
 
-    public static boolean isBelow(@NotNull VersionEnum ve) {
-    	return false;
-    }
+	public static boolean is(@NotNull VersionEnum ve) {
+		return false;
+	}
 
-    public static boolean isBetween(@NotNull VersionEnum ve1, @NotNull VersionEnum ve2) {
-    	return false;
-    }
 
-    public enum VersionEnum {
+	public static boolean isCompatible(@NotNull VersionEnum ve){
+		return false;
+	}
 
-       	V17(0),
-        V18(1),
-        V19(2),
-        V110(3),
-        V111(4),
-        V112(5),
-        V113(6),
-        V114(7),
-        V115(8),
-        V116(9),
-        V117(10),
-        V118(11),
-    	V119(12),
-    	V120(13),
-    	V121(14),
-    	V1211(15),
-    	V261(16);
+	public static boolean isAbove(@NotNull VersionEnum ve) {
+		return false;
+	}
+
+	public static boolean isBelow(@NotNull VersionEnum ve) {
+		return false;
+	}
+
+	public static boolean isBetween(@NotNull VersionEnum ve1, @NotNull VersionEnum ve2) {
+		return false;
+	}
+	
+	public static boolean isMinecraftFamily(@NotNull VersionEnum version) {
+		return false;
+	}
+
+	public enum VersionEnum {
+
+		V17(0),
+		V18(1),
+		V19(2),
+		V110(3),
+		V111(4),
+		V112(5),
+		V113(6),
+		V114(7),
+		V115(8),
+		V116(9),
+		V117(10),
+		V118(11),
+		V119(12),
+		V120(13),
+		V121(14),
+
+		// Versões específicas, caso ainda precise delas em outros lugares
+		V1211(15),
+
+		// Família Minecraft 26
+		V26(16),
+
+		V261(17),
+		V262(18);
 
         VersionEnum(int order) {
             
@@ -63,6 +76,5 @@ public class VersionUtil {
         public int getOrder() {
             return 0;
         }
-
-    }
+	}
 }
